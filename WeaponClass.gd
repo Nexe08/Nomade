afb6be4da3875e73826ee0cb2ac6921b18ecd98d
Nexe08 <87233162+Nexe08@users.noreply.github.com>
2021-07-10 19:22:32 +0530

    Add files via upload

---
+++ WeaponClass.gd
@@ -0,0 +1,14 @@
+extends Resource
+class_name WeaponClass
+
+export (String) var Name
+export (float) var damage
+export (float) var speed
+export (float) var fire_rate
+export (float) var life_time
+export (Texture) var projectile_texture
+export (String) var projectile_texture_path
+export (Texture) var icon_texture
+export (String) var icon_texture_path 
+export (Texture) var drop_texture
+export (String) var drop_texture_path
